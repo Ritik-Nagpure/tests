575dbdb6effb9e02a4eb6c219955eeb60a38ce05
Ritik Nagpure <80842165+Ritik-Nagpure@users.noreply.github.com>
2025-12-29 18:45:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 console.log("🚀 Starting the console app...");
 console.log("Hello, world!");
 console.log("This is a simple JS console application running without any inputs.");
+console.log("Beta Branch");
 
 num = Math.ceil(Math.random() * 40)
 console.log(`${num} * ${num} = ${num * num}`);
