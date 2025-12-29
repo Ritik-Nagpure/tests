d0c3f81f7f9c44d60759a4d802d25c3eca76b97a
Ritik Nagpure <80842165+Ritik-Nagpure@users.noreply.github.com>
2025-12-29 11:09:00 +0530

    trigger test 2

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ console.log("🚀 Starting the console app...");
 console.log("Hello, world!");
 console.log("This is a simple JS console application running without any inputs.");
 
-num = Math.ceil(Math.random() * 10)
+num = Math.ceil(Math.random() * 100)
 for (let i = 1; i < 11; i++) {
     console.log(`${num} * ${i} = ${num * i}`);
+}
+
+for (let i = 1; i < 11; i++) {
+    console.log(`${i} * ${i} = ${i * i}`);
 }
 
 console.log("✅ Program finished successfully.");
